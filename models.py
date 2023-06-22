b3f30af81d70f9a5837ce8b41c75806d17af5769
Oliver <oliver@MSI>
2023-06-22 15:36:47 -0700

    added post model

--- models.py
+++ models.py
@@ -12,8 +12,8 @@ class User(db.Model):
     __tablename__ = "users"
 
     id = db.Column(db.Integer, primary_key = True, autoincrement = True)
-    first_name = db.Column(db.String(30), nullable = False)
-    last_name = db.Column(db.String(30), nullable = False)
+    first_name = db.Column(db.String, nullable = False)
+    last_name = db.Column(db.String, nullable = False)
     image_url = db.Column(db.String, default = "https://static.vecteezy.com/system/resources/thumbnails/009/734/564/small/default-avatar-profile-icon-of-social-media-user-vector.jpg")
 
     @classmethod
@@ -26,3 +26,14 @@ class User(db.Model):
 
     def get_full_name(self):
         return f"{self.first_name} {self.last_name}"
+
+class Post(db.Model):
+    __tablename__ = 'posts'
+
+    id = db.Column(db.Integer, primary_key = True, autoincrement = True)
+    title = db.Column(db.Text(300), nullable = False)
+    content = db.Column(db.Text(40000), nullable = False)
+    created_at = db.Column(db.DateTime, nullable = False)
+    op = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+    user = db.relationship('User', backref='post')
